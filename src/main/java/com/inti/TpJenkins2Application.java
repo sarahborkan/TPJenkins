f7db34f2beda0ea4970be013823e920071db5724
sarahborkan <sarah.behaz1@gmail.com>
2023-06-29 11:33:02 +0200

    test

---
+++ src/main/java/com/inti/TpJenkins2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJenkins2Application.class, args);
+	}
+
+}
